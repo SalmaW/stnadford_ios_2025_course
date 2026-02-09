a627c9e28fa17be925d72027581585da8c7aa451
Salma <salmamaarouf00@gmail.com>
2026-02-09 03:11:41 +0200

    lec 4

---
+++ standford_course_2025/CodeBreakerView.swift
@@ -0,0 +1,72 @@
+//
+//  CodeBreakerView.swift
+//  standford_course_2025
+//
+//  Created by Wael Maarouf on 08/02/2026.
+//
+
+import SwiftUI
+
+struct CodeBreakerView: View {
+    @State var game = CodeBreaker(pegChoices: [
+        .brown, .yellow, .orange, .black,
+    ])
+
+    var body: some View {
+        VStack {
+            view(for: game.masterCode)
+            ScrollView {
+                view(for: game.guess)
+                ForEach(game.attempts.indices.reversed(), id: \.self) { index in
+                    view(for: game.attempts[index])
+                }
+                //            pegs(colors: game.attempts[0].pegs)
+            }
+
+        }
+        .padding()
+    }
+
+    var guessButton: some View {
+        Button("Guess") {
+            withAnimation {
+                game.attemptGuess()
+
+            }
+        }
+        .font(.system(size: 80))
+        .minimumScaleFactor(0.1)
+    }
+
+    func view(for code: Code) -> some View {
+        HStack {
+            ForEach(code.pegs.indices, id: \.self) { index in
+                RoundedRectangle(cornerRadius: 10)
+                    .overlay{
+                        if code.pegs[index] == Code.missing{
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.gray, lineWidth: 2)
+                        }
+                    }
+                    .contentShape(Rectangle())
+                    .aspectRatio(1, contentMode: .fit)
+                    .foregroundStyle(code.pegs[index])
+                    .onTapGesture {
+                        if code.kind == .guess {
+                            game.changeGuessPeg(at: index)
+                        }
+                    }
+            }
+            MatchMarkers(matches: code.matches)
+                .overlay {
+                    if code.kind == .guess {
+                        guessButton
+                    }
+                }
+        }
+    }
+}
+
+#Preview {
+    CodeBreakerView()
+}
--- standford_course_2025/standford_course_2025App.swift
+++ standford_course_2025/standford_course_2025App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct standford_course_2025App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CodeBreakerView()
         }
     }
 }
